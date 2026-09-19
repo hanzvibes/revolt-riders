335c2edfc616939b67cbc2b6c0d35b7d051f55c7
Raihan Ramadhani <64344985+hanzvibes@users.noreply.github.com>
2026-09-20 02:18:03 +0700

    Add symmetric collapsible motion

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -2,6 +2,8 @@
 
 import { Collapsible as CollapsiblePrimitive } from "radix-ui"
 
+import { cn } from "@/lib/utils"
+
 function Collapsible({
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
@@ -20,11 +22,16 @@ function CollapsibleTrigger({
 }
 
 function CollapsibleContent({
+  className,
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
   return (
     <CollapsiblePrimitive.CollapsibleContent
       data-slot="collapsible-content"
+      className={cn(
+        "overflow-hidden data-[state=closed]:animate-accordion-up data-[state=closed]:duration-[220ms] data-[state=closed]:ease-in data-[state=open]:animate-accordion-down data-[state=open]:duration-[240ms] data-[state=open]:ease-out",
+        className
+      )}
       {...props}
     />
   )
